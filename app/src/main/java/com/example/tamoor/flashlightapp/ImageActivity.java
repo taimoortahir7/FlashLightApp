cf82df197ed8613996cfa96dc863749212922c12
taimoor <bitf14a040@pucit.edu.pk>
2018-09-10 15:37:11 +0500

    changes in GIFs brightness

--- app/src/main/java/com/example/tamoor/flashlightapp/ImageActivity.java
+++ app/src/main/java/com/example/tamoor/flashlightapp/ImageActivity.java
@@ -1,6 +1,8 @@
 package com.example.tamoor.flashlightapp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +13,8 @@ public class ImageActivity extends AppCompatActivity {
     Context context;
     GifImageView imageView;
     int gifDrawableResource;
+    private static int brightness_value = 0;
+    private static float bright = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +22,48 @@ public class ImageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image);
 
         context = getBaseContext();
+        setBrightness(255);
         gifDrawableResource = getIntent().getIntExtra("image", 0);
         initialization();
         setImage();
+    }
+
+    public void setBrightness(int brightness){
+
+        //constrain the value of brightness
+        if(brightness < 0) {
+            brightness = 0;
+        }
+        else if(brightness > 255) {
+            brightness = 255;
+        }
+        bright = brightness;
+        getPreviousBrightness();
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            boolean settingsCanWrite = Settings.System.canWrite(getBaseContext());
+            if(!settingsCanWrite) {
+                // If do not have write settings permission then open the Can modify system settings panel.
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                startActivity(intent);
+            }
+            Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+            Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, brightness);
+
+        } else {
+            android.provider.Settings.System.putInt(getContentResolver(),
+                    android.provider.Settings.System.SCREEN_BRIGHTNESS,
+                    brightness);
+        }
+    }
+
+    private void getPreviousBrightness() {
+        try {
+            brightness_value = Settings.System.getInt(getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     private void initialization() {
@@ -29,5 +72,19 @@ public class ImageActivity extends AppCompatActivity {
 
     private void setImage() {
         imageView.setImageResource(gifDrawableResource);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+            Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, brightness_value);
+        } else {
+            android.provider.Settings.System.putInt(getContentResolver(),
+                    android.provider.Settings.System.SCREEN_BRIGHTNESS,
+                    brightness_value);
+        }
+
     }
 }
